ad752fbb905c68f74278b0e7cb6ac5baf2ff902b
Moses Palmér <moses@blockmastersecurity.com>
2014-06-02 11:26:40 +0200

    Added decorator for DNS driver tests

--- lib/tests/suites/driver.py
+++ lib/tests/suites/driver.py
@@ -1,10 +1,27 @@
 from .. import *
+from . import API_KEY, API_SECRET
+
+import functools
+import sys
+import types
 
 from libcloud.dns.providers import get_driver
 
 from dnsmadeeasy.driver import DNSMadeEasyDNSDriver
 
 Driver = get_driver('dnsmadeeasy')
+
+def drivertest(f):
+    """Marks a function as a test for the DNS driver"""
+    @functools.wraps(f)
+    def inner():
+        try:
+            return f(Driver(API_KEY, API_SECRET, True))
+        except DNSMadeEasyRateLimitExceededError as e:
+            printf('Rate limit exceeded (0 of %d requests remaining); '
+                    'terminating prematuely', e.request_limit)
+            sys.exit(2)
+    return test(inner)
 
 
 @test
